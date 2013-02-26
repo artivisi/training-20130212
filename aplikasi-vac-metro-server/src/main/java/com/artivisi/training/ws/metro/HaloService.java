caef07c2b1d48326a4af0744a9fcbdfe51f4e87b
Endy Muhardin <endy.muhardin@gmail.com>
2013-02-26 15:00:01 +0700

    menambahkan Spring Support di WS Metro

--- aplikasi-vac-metro-server/src/main/java/com/artivisi/training/ws/metro/HaloService.java
+++ aplikasi-vac-metro-server/src/main/java/com/artivisi/training/ws/metro/HaloService.java
@@ -4,16 +4,24 @@
  */
 package com.artivisi.training.ws.metro;
 
+import com.artivisi.training.dao.RoleDao;
+import com.artivisi.training.domain.Role;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 /**
  *
  * @author endy
  */
 @WebService(serviceName = "HaloService")
-public class HaloService {
+public class HaloService extends SpringBeanAutowiringSupport {
+    
+    @Autowired
+    private RoleDao roleDao;
 
     /**
      * This is a sample web service operation
@@ -21,5 +29,20 @@ public class HaloService {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "cariSemuaRole")
+    public List<Role> cariSemuaRole() {
+        List<Role> hasil = roleDao.cariSemuaRole();
+        
+        for (Role role : hasil) {
+            role.setDaftarPermission(null);
+            role.setDaftarUser(null);
+        }
+        
+        return hasil;
     }
 }
